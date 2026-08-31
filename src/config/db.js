1bcce367942cd10daab166bb615948c462da4ea4
codephoenix86 <nareshlohar066@gmail.com>
2026-08-31 19:54:53 +0000

    feat: configure postgresql and prisma

---
+++ src/config/db.js
@@ -0,0 +1,8 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from '../../generated/prisma/client.ts';
+import { env } from './env.js';
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+export const db = new PrismaClient({ adapter });
